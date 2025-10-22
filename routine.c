4404575df2582c25a6794d03f13c3c8a21f3c151
xmaram42 <xmaram42@gmail.com>
2025-10-22 16:50:21 +0000

    ll

--- routine.c
+++ routine.c
@@ -16,14 +16,14 @@ void	handle_single_philo(t_philo *philo)
 {
 	pthread_mutex_lock(philo->left_fork);
 	print_state(philo, "has taken a fork 🍴");
-	usleep_ms(philo->data->die,  philo->data);
+	usleep_ms(philo->data->die, philo->data);
 	pthread_mutex_unlock(philo->left_fork);
 }
 
 void	print_state(t_philo *philo, char *msg)
 {
 	long long	timestamp;
-	int			stop;
+	int		stop;
 
 	pthread_mutex_lock(&philo->data->print_lock);
 	timestamp = get_time_ms() - philo->data->start_time;
@@ -35,65 +35,71 @@ void	print_state(t_philo *philo, char *msg)
 	pthread_mutex_unlock(&philo->data->print_lock);
 }
 
-int	take_forks(t_philo *philo)
+static void	set_hold(t_philo *philo, pthread_mutex_t *fork, int status)
 {
-	pthread_mutex_t *first;
-	pthread_mutex_t *second;
+	if (fork == philo->left_fork)
+		philo->holding_left = status;
+	else
+		philo->holding_right = status;
+}
 
+static void	choose_order(t_philo *philo, pthread_mutex_t **first,
+			pthread_mutex_t **second)
+{
 	if (philo->id % 2 == 0)
 	{
-		first = philo->right_fork;
-		second = philo->left_fork;
+		*first = philo->right_fork;
+		*second = philo->left_fork;
 	}
 	else
 	{
-		first = philo->left_fork;
-		second = philo->right_fork;
+		*first = philo->left_fork;
+		*second = philo->right_fork;
 	}
-	pthread_mutex_lock(first);
-	if (first == philo->left_fork)
-		philo->holding_left = 1;
-	else
-		philo->holding_right = 1;
-	print_state(philo, "has taken a fork 🍴");
-	if (first == second)
-	{
-		pthread_mutex_unlock(first);
-		if (first == philo->left_fork)
-			philo->holding_left = 0;
-		else
-			philo->holding_right = 0;
-		return (0);
-	}
-	if (simulation_stopped(philo->data))
-	{
-		pthread_mutex_unlock(first);
-		if (first == philo->left_fork)
-			philo->holding_left = 0;
-		else
-			philo->holding_right = 0;
-		return (0);
-	}
-	pthread_mutex_lock(second);
-	if (second == philo->left_fork)
-		philo->holding_left = 1;
-	else
-		philo->holding_right = 1;
+}
+
+static int	lock_fork(t_philo *philo, pthread_mutex_t *fork)
+{
+	pthread_mutex_lock(fork);
+	set_hold(philo, fork, 1);
 	print_state(philo, "has taken a fork 🍴");
 	if (simulation_stopped(philo->data))
 	{
-		pthread_mutex_unlock(second);
-		if (first == philo->left_fork || second == philo->left_fork)
-			philo->holding_left = 0;
-		if (first == philo->right_fork || second == philo->right_fork)
-			philo->holding_right = 0;
-		pthread_mutex_unlock(first);
+		pthread_mutex_unlock(fork);
+		set_hold(philo, fork, 0);
 		return (0);
 	}
 	return (1);
 }
 
-int eat_meal(t_philo *philo)
+static void	release_fork(t_philo *philo, pthread_mutex_t *fork)
+{
+	pthread_mutex_unlock(fork);
+	set_hold(philo, fork, 0);
+}
+
+int	take_forks(t_philo *philo)
+{
+	pthread_mutex_t	*first;
+	pthread_mutex_t	*second;
+
+	choose_order(philo, &first, &second);
+	if (!lock_fork(philo, first))
+		return (0);
+	if (first == second)
+	{
+		release_fork(philo, first);
+		return (0);
+	}
+	if (!lock_fork(philo, second))
+	{
+		release_fork(philo, first);
+		return (0);
+	}
+	return (1);
+}
+
+int	eat_meal(t_philo *philo)
 {
 	print_state(philo, "is eating 🍝");
 	pthread_mutex_lock(&philo->meal_lock);
@@ -112,33 +118,30 @@ int eat_meal(t_philo *philo)
 	return (1);
 }
 
-
-void	*philo_routine(void *ar)
+static void	sync_start(t_philo *philo)
 {
-	t_philo	*philo;
-
-	philo = (t_philo *)ar;
-	if (philo->left_fork == philo->right_fork)
-	{
-		print_state(philo, "is thinking 💭");
-		return (handle_single_philo(philo), NULL);
-	}
 	if (philo->id % 2 == 0)
 		usleep_ms(philo->data->eat / 2, philo->data);
 	else if (philo->data->n % 2 != 0)
 		usleep_ms(philo->data->eat, philo->data);
-	while (!simulation_stopped(philo->data))
-	{
-		print_state(philo, "is thinking 💭");
-		if (!take_forks(philo))
-			break ;
-		if (!eat_meal(philo))
-			break ;
-                print_state(philo, "is sleeping 😴");
-                usleep_ms(philo->data->sleep, philo->data);
-                if (philo->data->n % 2 == 0)
-                        usleep_ms(1, philo->data);
-	}
+}
+
+static int	philo_cycle(t_philo *philo)
+{
+	print_state(philo, "is thinking 💭");
+	if (!take_forks(philo))
+		return (0);
+	if (!eat_meal(philo))
+		return (0);
+	print_state(philo, "is sleeping 😴");
+	usleep_ms(philo->data->sleep, philo->data);
+	if (philo->data->n % 2 == 0)
+		usleep_ms(1, philo->data);
+	return (1);
+}
+
+static void	release_remaining(t_philo *philo)
+{
 	if (philo->holding_right)
 	{
 		pthread_mutex_unlock(philo->right_fork);
@@ -149,6 +152,25 @@ void	*philo_routine(void *ar)
 		pthread_mutex_unlock(philo->left_fork);
 		philo->holding_left = 0;
 	}
-	return (NULL);
 }
 
+void	*philo_routine(void *ar)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)ar;
+	if (philo->left_fork == philo->right_fork)
+	{
+		print_state(philo, "is thinking 💭");
+		handle_single_philo(philo);
+		return (NULL);
+	}
+	sync_start(philo);
+	while (!simulation_stopped(philo->data))
+	{
+		if (!philo_cycle(philo))
+			break ;
+	}
+	release_remaining(philo);
+	return (NULL);
+}
